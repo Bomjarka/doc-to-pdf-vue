09b19baa3f58e0a1615f8c6bf801c5b9fd899d4c
a.chirkin <a.chirkin@robo.finance>
2023-06-28 15:53:04 +0700

    add converter package to project

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DocumentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('migration', function () {
     return DB::table('migrations')->get();
 });
+
+Route::post('get-vars', [DocumentController::class, 'getVars'])->name('get-vars');
+
+Route::get('convert', [DocumentController::class, 'convert'])->name('convert');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
