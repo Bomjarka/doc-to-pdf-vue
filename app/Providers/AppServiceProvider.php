087efe04d17c1a67bcdd4ac5f659fe952353cdef
a.chirkin <a.chirkin@robo.finance>
2023-07-04 18:08:36 +0700

    update converter package

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,7 @@
 
 namespace App\Providers;
 
-use Bomjarka\WordToPdfConverter\Services\Converter\ConverterInterface;
-use Bomjarka\WordToPdfConverter\Services\Converter\PDFConverter;
-use Bomjarka\WordToPdfConverter\Services\Repository\FileSystemRepository;
-use Bomjarka\WordToPdfConverter\Services\Repository\RepositoryInterface;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +14,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(ConverterInterface::class, PDFConverter::class);
-        $this->app->bind(RepositoryInterface::class, FileSystemRepository::class);
     }
 
     /**
